7dfaa2ba57d9b35e87c796478fe9c7960b517667
tmaulon <thomas.maulon@gmail.com>
2019-11-27 22:54:03 +0100

    add markdown with localStorage handling data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import marked from "marked";
+
+import { sampleText } from "./sampleText";
+
+class App extends Component {
+  state = {
+    text: sampleText
+  };
+
+  componentDidMount() {
+    const text = localStorage.getItem("text");
+    if (text) {
+      this.setState({ text });
+    } else {
+      this.setState({ text: sampleText });
+    }
+  }
+
+  componentDidUpdate() {
+    const { text } = this.state;
+    localStorage.setItem("text", text);
+  }
+
+  handleChange = event => {
+    const text = event.target.value;
+    this.setState({ text });
+  };
+
+  renderText = text => {
+    const __html = marked(text, { sanitize: true });
+    return { __html };
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <textarea
+              rows="35"
+              className="form-control"
+              onChange={this.handleChange}
+              value={this.state.text}
+            ></textarea>
+          </div>
+          <div className="col-sm-6">
+            <div dangerouslySetInnerHTML={this.renderText(this.state.text)} />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
